22ddc892ddac559dadae0d0c33f77b87429cee19
strangersj <strangersj@outlook.com>
2019-12-15 22:30:23 +0800

    Geo数据请求接口

--- server/routes/data.js
+++ server/routes/data.js
@@ -8,29 +8,63 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-// 获取温度梯度数据，接收传递参数date为请求的日期
-router.post('/grid', (req, res, next) => {
-    //let collectionName = req.query.type; // 请求的时间分辨率，不同力度在不同的集合下
-    let collectionName = req.query.date; // 获取请求的日期
-    let query = { "sstg": {$ne: NaN} };
-
+router.post('/geo', (req, res, next) => {
+    // type与集合名相同
+    let collectionName = req.query.type; // 获取请求数据类型，分为海区和数据点两个Geo，分别对应集合：zone、dataPoly
+    let query = {}; // 查询条件为查询所有
+    // 查询数据库
     DB.find(collectionName, query, (err, docs) => {
-        let oceanInfo = [];
+        let geoInfo = []; // geo数据数组
         if (err) {
+            // 失败的回调
             res.json({
                 status: 1,
                 data: err.message
             });
         } else {
+            //成功的回调
             for (let i = 0; i < docs.length; i++){
+                geoInfo.push({
+                    properties: docs[i].properties,
+                    type: docs[i].type,
+                    geometry: docs[i].geometry
+                });
+            }
+            // 返回数据
+            res.json({
+                status: 0,
+                message: {
+                    length: geoInfo.length,
+                    data: geoInfo
+                }
+            })
+        }
+        
+    });
+});
+
+// 获取温度梯度数据，接收传递参数date为请求的日期
+router.post('/grid', (req, res, next) => {
+    let collectionName = req.query.date; // 获取请求的日期
+    let query = { "sstg": {$ne: NaN} };
+    // 请求数据库
+    DB.find(collectionName, query, (err, docs) => {
+        let oceanInfo = []; // 返回的数据数组
+        if (err) { // 数据库查询失败回调
+            res.json({
+                status: 1,
+                data: err.message
+            });
+        } else { // 成功的回调
+            for (let i = 0; i < docs.length; i++){ // 遍历文档，组织数据结构
                 oceanInfo.push({
-                    'latitude': parseFloat(docs[i].latitude),
-                    'longitude': parseFloat(docs[i].longitude),
-                    'sstg': parseFloat(docs[i].sstg)
+                    latitude: parseFloat(docs[i].latitude),
+                    longitude: parseFloat(docs[i].longitude),
+                    sstg: parseFloat(docs[i].sstg)
                 });
             }
 
-            res.json({
+            res.json({  // 返回数据
                 status: 0,
                 message: {
                     count: oceanInfo.length,                    
