5d76a42ef4a08455ce6fd1582290eeb3b578d3bf
strangersj <strangersj@outlook.com>
2019-12-21 22:49:25 +0800

    雷达图组件

---
+++ client/src/components/charts/Radar.vue
@@ -0,0 +1,77 @@
+<template>
+	<v-chart :options="radarOpt" autoresize></v-chart>
+</template>
+
+<script>
+import ECharts from 'vue-echarts';
+import 'echarts/lib/chart/radar';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/legend';
+import 'echarts/lib/component/radar';
+
+export default {
+	data() {
+		return {
+            radarOpt: {
+                title: {
+                    text: '基础雷达图'
+                },
+                tooltip: {},
+                legend: {
+                    data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+                },
+                radar: {
+                    // shape: 'circle',
+                    name: {
+                        textStyle: {
+                            color: '#fff',
+                            backgroundColor: '#999',
+                            borderRadius: 3,
+                            padding: [3, 5]
+                    }
+                    },
+                    indicator: [
+                    { name: '销售（sales）', max: 6500},
+                    { name: '管理（Administration）', max: 16000},
+                    { name: '信息技术（Information Techology）', max: 30000},
+                    { name: '客服（Customer Support）', max: 38000},
+                    { name: '研发（Development）', max: 52000},
+                    { name: '市场（Marketing）', max: 25000}
+                    ]
+                },
+                series: [{
+                    name: '预算 vs 开销（Budget vs spending）',
+                    type: 'radar',
+                    // areaStyle: {normal: {}},
+                    data : [
+                        {
+                            value : [4300, 10000, 28000, 35000, 50000, 19000],
+                            name : '预算分配（Allocated Budget）'
+                        },
+                        {
+                            value : [5000, 14000, 28000, 31000, 42000, 21000],
+                            name : '实际开销（Actual Spending）'
+                        }
+                    ]
+                }]
+            }
+        };
+	},
+	watch: {},
+	methods: {},
+	created() {},
+    mounted() {},
+    components:{
+        'v-chart': ECharts
+    },
+	props: {}
+};
+</script>
+
+<style scoped>
+.echarts {
+	width: 100%;
+	height: 100%;
+}
+</style>
