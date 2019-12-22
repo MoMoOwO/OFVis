de5daee9cfd12da5765d67c43209d75e84ad92a9
strangersj <strangersj@outlook.com>
2019-12-22 23:03:26 +0800

    完成雷达图开发

--- client/src/components/charts/Radar.vue
+++ client/src/components/charts/Radar.vue
@@ -1,5 +1,10 @@
 <template>
-	<v-chart :options="radarOpt" autoresize></v-chart>
+	<v-chart 
+        ref="radar" 
+        :options="radarOpt" 
+        autoresize 
+        @click="handleClick">
+    </v-chart>
 </template>
 
 <script>
@@ -15,11 +20,28 @@ export default {
 		return {
             radarOpt: {
                 title: {
-                    text: '基础雷达图'
+                    text: 'Area',
+                    subtext: ''
                 },
-                tooltip: {},
+                tooltip: {
+                    //雷达图的tooltip不会超出div，也可以设置position属性，position定位的tooltip 不会随着鼠标移动而位置变化，不友好
+                    confine: true,
+                    formatter: params => {
+                        // 由于雷达图事逆时针排列，所以实际顺序事1月12月11月....
+                        let info = `${params.name}: <br />${this.radarOpt.radar.indicator[0].text}: ${params.value[0].toFixed(2)}<br />`;
+                        for (let i = params.value.length - 1; i > 0 ; i--){
+                            info += `${this.radarOpt.radar.indicator[i].text}: ${params.value[i].toFixed(2)} km² <br />`;
+                        }
+                        return info;
+					}
+                },
                 legend: {
-                    data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+                    orient: 'vertical',
+                    data: [
+                        { name: '2015', icon: 'circle', color: 'rgb(228, 26, 28)' },
+                        { name: '2016', icon: 'circle', color: 'rgb(55, 126, 184)' },
+                        { name: '2017', icon: 'circle', color: 'rgb(77, 175, 74)' }
+                    ]
                 },
                 radar: {
                     // shape: 'circle',
@@ -29,29 +51,55 @@ export default {
                             backgroundColor: '#999',
                             borderRadius: 3,
                             padding: [3, 5]
-                    }
+                        }
                     },
+                    //triggerEvent: true, // 坐标轴标签响应和触发鼠标事件
                     indicator: [
-                    { name: '销售（sales）', max: 6500},
-                    { name: '管理（Administration）', max: 16000},
-                    { name: '信息技术（Information Techology）', max: 30000},
-                    { name: '客服（Customer Support）', max: 38000},
-                    { name: '研发（Development）', max: 52000},
-                    { name: '市场（Marketing）', max: 25000}
+                        { text: "Jan", max: 150,
+                            axisLabel: {
+                            show: true,
+                            formatter: '{value} km²',
+                            color: 'black'
+                        }},
+                        { text: "Dec", max: 150 },
+                        { text: "Nov", max: 150 },
+                        { text: "Oct", max: 150 },
+                        { text: "Sep", max: 150 },
+                        { text: "Aug", max: 150 },
+                        { text: "Jul", max: 150 },
+                        { text: "Jun", max: 150 },
+                        { text: "May", max: 150 },
+                        { text: "Apr", max: 150 },
+                        { text: "Mar", max: 150 },
+                        { text: "Feb", max: 150 }
                     ]
                 },
                 series: [{
-                    name: '预算 vs 开销（Budget vs spending）',
+                    name: 'Area',
                     type: 'radar',
+                    symbol: 'circle', // 拐点的样式，还可以取值'rect','angle'等
+                    symbolSize: 8, // 拐点的大小
+                    itemStyle: {
+                        emphasis: {
+                            // color: 各异,
+                            lineStyle: {
+                                width: 4
+                            }
+                        }
+                    },
                     // areaStyle: {normal: {}},
                     data : [
                         {
-                            value : [4300, 10000, 28000, 35000, 50000, 19000],
-                            name : '预算分配（Allocated Budget）'
+                            name: '2015',
+                            value: [110, 100, 90, 80, 120, 130, 60, 80, 120, 130, 120, 70]
                         },
                         {
-                            value : [5000, 14000, 28000, 31000, 42000, 21000],
-                            name : '实际开销（Actual Spending）'
+                            name:'2016',
+                            value:[105, 96, 89, 110, 90, 95, 59, 50, 116, 73, 57, 89]
+                        },
+                        {
+                            name:'2017',
+                            value:[118, 93, 86, 128, 86, 120, 70, 59, 127, 80, 70, 97]
                         }
                     ]
                 }]
@@ -59,7 +107,65 @@ export default {
         };
 	},
 	watch: {},
-	methods: {},
+	methods: {
+        handleClick(e){
+            // 点击拐点的索引
+            let index = e.event.target.__dimIdx;
+            // subtext显示三个年份该月的面积
+            let info = this.radarOpt.radar.indicator[index].text + ': ';
+            let dataArr = this.radarOpt.series[0].data;
+            for (let i = 0; i < dataArr.length; i++){
+                info += `${dataArr[i].name}: ${dataArr[i].value[index]}  `;
+            }
+            this.radarOpt.title.subtext = info;
+
+            // 更新地图显示日期
+            let month = this.radarOpt.radar.indicator[index].text;
+            switch (month) {
+                case "Jan":
+                    month = '01';
+                    break;
+                case "Feb":
+                    month = "02";
+                    break;
+                case "Mar":
+                    month = "03";
+                    break;
+                case "Apr":
+                    month = '04';
+                    break;
+                case "May":
+                    month = "05";
+                    break;
+                case "Jun":
+                    month = "06";
+                    break;
+                case "Jul":
+                    month = '07';
+                    break;
+                case "Aug":
+                    month = "08";
+                    break;
+                case "Sep":
+                    month = "09";
+                    break;
+                case "Oct":
+                    month = '10';
+                    break;
+                case "Nov":
+                    month = "11";
+                    break;
+                case "Dec":
+                    month = "12";
+                    break;
+                default:
+                    month = "01";
+                    break;
+            }
+            // 通过调用父组件的方法给父组件传值(修改data中的值)
+			this.$emit('changeDateInMap', +(e.name + month));
+        }
+    },
 	created() {},
     mounted() {},
     components:{
