e49b00e967895e8dbece704748706f56a0e8010d
strangersj <strangersj@outlook.com>
2019-12-25 22:03:14 +0800

    想象图组件

--- client/src/components/charts/Box.vue
+++ client/src/components/charts/Box.vue
@@ -1,30 +1,26 @@
 <template>
 	<v-chart
         ref="box" 
+        autoresize
         :options="boxOpt">
-        </v-chart>
+    </v-chart>
 </template>
 
 <script>
 import ECharts from 'vue-echarts';
 import 'echarts/lib/chart/boxplot';
+import 'echarts/lib/chart/scatter';
 import 'echarts/lib/component/title';
 import 'echarts/lib/component/tooltip';
 
 export default {
 	data() {
 		return {
-            data: this.ECharts.dataTool.prepareBoxplotData([
-                [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
-                [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
-                [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
-                [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
-                [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
-            ]),
+            date: '20150101',
             boxOpt: {
                 title: [
                     {
-                        text: 'Michelson-Morley Experiment',
+                        text: 'Stranger',
                         left: 'center',
                     },
                     {
@@ -51,7 +47,7 @@ export default {
                 },
                 xAxis: {
                     type: 'category',
-                    data: data.axisData,
+                    data: null,
                     boundaryGap: true,
                     nameGap: 30,
                     splitArea: {
@@ -75,7 +71,7 @@ export default {
                     {
                         name: 'boxplot',
                         type: 'boxplot',
-                        data: data.boxData,
+                        data: null,
                         tooltip: {
                             formatter: function (param) {
                                 return [
@@ -92,16 +88,100 @@ export default {
                     {
                         name: 'outlier',
                         type: 'scatter',
-                        data: data.outliers
+                        data: null
                     }
                 ]
             }
         };
 	},
 	watch: {},
-	methods: {},
+	methods: {
+        getData(){
+            this.axios
+                .post(`data/box?date=${this.date}`)
+                .then( result => {
+                    if (result.data.status === 0) {
+                        console.log(result);
+                        let dataShow = result.data.message;
+                        this.boxOpt.xAxis.data = dataShow.axisData;
+                        this.boxOpt.series[0].data = dataShow.boxData;
+                        this.boxOpt.series[1].data = dataShow.outliers;
+						// 隐藏缓冲条
+						this.$refs.box.hideLoading();
+					} else {
+						// 失败请求数据的回调
+						this.$notify.error({
+							title: "Error",
+							message: "Failed get Calendar-data!"
+						});
+					}
+                });
+        },
+        quantile(arr, per){
+            let i = (arr.length - 1) * per + 1, n = Math.floor(i), a = +arr[n - 1], o = i - n;
+            return o ? a + o * (arr[n] - a) : a;
+        },
+        prepareBoxPlotData(rawData, opt){
+            opt = opt || [];
+            let boxData = []; // 箱图中的数据
+            let outliers = []; // 离群值
+            let axisData = []; // 坐标轴的值
+            let boundIQR = opt.boundIQR; // 获取 boundIQR
+            let useExtreme = boundIQR === 'none' || boundIQR === 0; // 是否使用界限
+
+            for (let i = 0; i < rawData.length; i++) {
+                axisData.push(i + ''); // 坐标轴序号
+                let ascList = rawData[i].sort((a, b) => a - b ); // 获取升序序数组
+
+                // 下四分位数
+                let Q1 = this.quantile(ascList, 0.25);
+                // 中位数
+                let Q2 = this.quantile(ascList, 0.5);
+                // 上四分位数
+                let Q3 = this.quantile(ascList, 0.75); 
+                let min = ascList[0]; // 最小值
+                let max = ascList[ascList.length - 1]; // 最大值
+
+                let bound = (boundIQR == null ? 1.5 : boundIQR) * (Q3 - Q1); // 设定的边界范围差
+
+                // 下边缘
+                let low = useExtreme
+                    ? min
+                    : Math.max(min, Q1 - bound);
+                // 上边缘
+                let high = useExtreme
+                    ? max
+                    : Math.min(max, Q3 + bound);
+
+                boxData.push([low, Q1, Q2, Q3, high]); // 箱线图数据
+
+                // 异常值数据
+                for (let j = 0; j < ascList.length; j++) {
+                    let dataItem = ascList[j];
+                    if (dataItem < low || dataItem > high) {
+                        let outlier = [i, dataItem];
+                        opt.layout === 'vertical' && outlier.reverse();
+                        outliers.push(outlier);
+                    }
+                }
+            }
+            return {
+                boxData: boxData,
+                outliers: outliers,
+                axisData: axisData
+            };
+        }
+    },
 	created() {},
-	mounted() {},
+	mounted() {
+        // 显示缓冲条
+		this.$refs.box.showLoading({
+			text: 'Loading…',
+			color: '#F03B20',
+			maskColor: 'rgba(255, 255, 255, 0.4)'
+		});
+        this.getData();
+    },
     props: {},
     components: {
         'v-chart': ECharts
