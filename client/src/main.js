2c05a9952265698f9ee4ee05e32d85016a22f341
strangersj <strangersj@outlook.com>
2020-01-06 22:59:00 +0800

    ElementUI国际化

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,9 @@ import router from './router';
 
 // 安装及配置Emement-UI
 import ElementUI from 'element-ui';
-Vue.use(ElementUI);
+// 引入国际化语言
+import locale from '../node_modules/element-ui/lib/locale/lang/en';
+Vue.use(ElementUI, {locale});
 // 引入ElementUI样式
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -33,11 +35,17 @@ var store = new Vuex.Store({
     // state可以理解为专门用来存储共享数据的
     // 如果在组件中访问state中的属性，只能通过this.$store.state.***来访问
     date: 20150101,
+    dateRange: ['2016-01-01', '2016-12-31']
   },
   mutations: {
     // 如果要操作store.state中的数据，只能通过调用mutatuions提供的方法才能操作对应的数据，不推荐直接操作state中的数据
+    // 修改单个日期，主要用于在地图上显示日期
     changeDate(state, newDate) {
       state.date = newDate;
+    },
+    // 修改日期范围，主要用于日历图联动到地图等
+    changeDateRange(state, newDate) {
+      state.dateRange = newDate;
     }
   }
 })
