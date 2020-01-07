bc67be83de91aeccb67a07a167ff3fbc2d489893
strangersj <strangersj@outlook.com>
2020-01-07 22:07:49 +0800

    错误修改

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-// 引入自定义组件
-import CalendarChart from '@/components/charts/Calendar.vue';
 
 Vue.use(Router)
 
@@ -11,7 +9,6 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-    },
-    {path: '/calendar', component: CalendarChart}
+    }
   ]
 })
