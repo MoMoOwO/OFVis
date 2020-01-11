2509fe6b6c8fae71da684bc513b16ee2303d709d
strangersj <strangersj@outlook.com>
2020-01-11 20:32:27 +0800

    使用v-gallery来设计空间画廊

---
+++ client/src/components/SpaceGallery.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-gallery 
+        :images="list" 
+        :caption="true">
+    </v-gallery>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+            list: [
+                {title: '1', url: require('./data/20160101.png')},
+                {title: '2', url: require('./data/201601.png')},
+                {title: '3', url: require('./data/20160101-b.png')}
+            ]
+        };
+	},
+	watch: {},
+	methods: {},
+	created() {},
+	mounted() {},
+    props: {},
+    components: {
+    }
+};
+</script>
+
+<style scoped>
+</style>
