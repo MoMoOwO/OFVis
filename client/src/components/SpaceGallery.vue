660038ce3aac5d4c5538708ffc9935ef38da6895
strangersj <strangersj@outlook.com>
2020-01-14 09:42:23 +0800

    熟悉v-gallery组件使用

--- client/src/components/SpaceGallery.vue
+++ client/src/components/SpaceGallery.vue
@@ -1,7 +1,11 @@
 <template>
+    <!-- type:gallery画廊/carousel走马灯， control-pause:走马灯状态时是否显示暂停播放按钮
+        dark:暗色主题，caption:画廊模式中是否展示图片描述  control-title:图片轮播或画廊全屏展示时受否显示图片描述 -->
 	<v-gallery 
         :images="list" 
-        :caption="true">
+        type="carousel"
+        :caption="true" 
+        :control-pause="true">
     </v-gallery>
 </template>
 
