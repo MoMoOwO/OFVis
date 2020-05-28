cc802026ccba93b6f276e8b85a37b5defddd1af0
strangersj <strangersj@outlook.com>
2020-05-28 20:30:13 +0800

    添加 element-ui 依赖

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入 element-ui 组件相关样式
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
