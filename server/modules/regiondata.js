25a65db2b7cbe3a650c22c6888b815209fc7fa1d
strangersj <strangersj@outlook.com>
2020-06-17 21:56:58 +0800

    封装db数据库

---
+++ server/modules/regiondata.js
@@ -0,0 +1,25 @@
+// 引入连接到数据库的 mongoose 对象
+const mongoose = require('./db.js');
+
+// 定义文档 RegionData 的 Schema
+const RegionDataSchema = mongoose.Schema({
+  date: Number,
+  FeaturesData: [
+    {
+      regionId: Number,
+      RipleyK: Number,
+      MoranI: Number,
+      Mean: Number,
+      Mode: Number,
+      Median: Number,
+      Range: Number,
+      Qd: Number,
+      Skewness: Number,
+      Excess_Kurtosis: Number,
+      Area: Number
+    }
+  ]
+});
+
+// 使用 Schema 创建可以操做改文档的 Model 并暴漏出去
+module.exports = mongoose.model("RegionData", RegionDataSchema, "RegionData");
