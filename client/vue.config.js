81c48e2f54ccf990f4836c66a85ee118330306fa
strangersj <strangersj@outlook.com>
2020-06-19 16:09:34 +0800

    配置代理解决扩域请求问题

---
+++ client/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    // 以上的 ip 和端口是我们本机的;下面为需要跨域的
+    proxy: {  // 配置跨域
+      '/api': {
+        target: 'http://localhost:3000/data/',  // 后台服务器地址
+        ws: true,
+        changOrigin: true,  // 允许跨域
+        pathRewrite: {
+          '^/api': ''  // 请求的时候使用这个 api 就可以
+        }
+      }
+    }
+  }
+}
