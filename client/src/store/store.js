d41452e91117aefb1c06b0b2a70645503e3f8826
strangersj <strangersj@outlook.com>
2020-06-25 21:51:05 +0800

    组件联动交互

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: { // 共享状态/数据
     barDateChoosed: '201501',
-    boxRegionChoosed: 'all'
+    boxRegionChoosed: 'all',
+    imgShowOnMap: '',
+    markersShowOnMap: []
   },
   mutations: { // 对外提供修改 state 中数据的接口
     selectedRegionIDOnBox(state, newID) {
@@ -14,6 +16,9 @@ const store = new Vuex.Store({
     },
     selectedDateOnBar(state, newVal) {
       state.barDateChoosed = newVal
+    },
+    selectImgShowOnMap(state, newVal) {
+      state.imgShowOnMap = newVal
     }
   },
   getters: { // 对外提供获取 state 中数据的接口，可以在获取时对 state 中数据进行包装，否则也可以直接从 state 中获取数据
