b2b4cda3e5b08110218096cdbef01050ec0b661d
strangersj <strangersj@outlook.com>
2020-06-26 21:00:58 +0800

    store共享数据更新

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: { // 共享状态/数据
-    barDateChoosed: '201501',
+    barDateChoosed: '201501', // 从面积柱状图中点击某个月份，箱线图切换到相同月份的统计，
     boxRegionChoosed: 'all',
+    galleryDateRange: '201501',
     imgShowOnMap: '',
-    markersShowOnMap: []
+    markerShowOnMap: [], // 箱线图悬浮的某个点
+    markersShowOnMap: [] // 箱线图中点选的点
   },
   mutations: { // 对外提供修改 state 中数据的接口
     selectedRegionIDOnBox(state, newID) {
@@ -16,9 +18,13 @@ const store = new Vuex.Store({
     },
     selectedDateOnBar(state, newVal) {
       state.barDateChoosed = newVal
+      state.galleryDateRange = newVal
     },
     selectImgShowOnMap(state, newVal) {
       state.imgShowOnMap = newVal
+    },
+    hoverPointOnBoxplot(state, newVal) {
+      state.markerShowOnMap = newVal
     }
   },
   getters: { // 对外提供获取 state 中数据的接口，可以在获取时对 state 中数据进行包装，否则也可以直接从 state 中获取数据
