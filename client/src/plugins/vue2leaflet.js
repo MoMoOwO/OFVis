1d30f54d632b893633a7ffd7b0ff5d8d94497155
strangersj <strangersj@outlook.com>
2020-07-04 21:19:36 +0800

    geojson control 组件引入

--- client/src/plugins/vue2leaflet.js
+++ client/src/plugins/vue2leaflet.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 // 按需引入 Vue2Leaflet 组件
-import { LMap, LTileLayer, LImageOverlay, LMarker, LTooltip } from 'vue2-leaflet'
+import { LMap, LTileLayer, LImageOverlay, LMarker, LTooltip, LGeoJson, LControl } from 'vue2-leaflet'
 // 将组建添加到 Vue 实例中
 Vue.component('l-map', LMap)
 Vue.component('l-tile-layer', LTileLayer)
 Vue.component('l-image-overlay', LImageOverlay)
 Vue.component('l-marker', LMarker)
 Vue.component('l-tooltip', LTooltip)
+Vue.component('l-geo-json', LGeoJson)
+Vue.component('l-control', LControl)
