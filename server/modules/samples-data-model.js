aff48f65cce96175124acfe0df2ac651b2b5b52a
strangersj <strangersj@outlook.com>
2020-12-12 20:09:40 +0800

    SamplesData Model

---
+++ server/modules/samples-data-model.js
@@ -0,0 +1,25 @@
+// 引入连接到数据库的 mongoose 对象
+const mongoose = require('./db.js');
+
+// 定义文档 RegionData 的 Schema
+const SamplesDataSchema = mongoose.Schema({
+  date: Number,
+  features: [
+    {
+      regionId: Number,
+      IQR: Number,
+      Skewness: Number,
+      MoransI: Number,
+      SDD: Number,
+      LALSR: Number,
+      Mean: Number,
+      Mode: Number,
+      Median: Number,
+      Excess_Kurtosis: Number,
+      Area: Number
+    }
+  ]
+});
+
+// 使用 Schema 创建可以操做改文档的 Model 并暴漏出去
+module.exports = mongoose.model("SamplesData", SamplesDataSchema, "SamplesData");
