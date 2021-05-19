92525e0556f3ba964aeb7c6071b772455fa7cb79
strangersj <strangersj@outlook.com>
2021-05-19 15:36:12 +0800

    以路由链接方式重构

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Analysis from '../components/Analysis.vue'
 import Detect from '../components/Detect.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/home' },
-  { path: '/home', component: Home },
-  { path: '/detect', component: Detect }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/detect', component: Detect },
+      { path: '/analysis', component: Analysis }
+    ]
+  }
 ]
 
 const router = new VueRouter({
